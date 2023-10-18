a8e6538df003d60c544f16137d9164a1b952b428
JeonMinsu <wjsrl528@naver.com>
2023-10-18 15:19:35 +0900

    add UI

--- hpe2d.py
+++ hpe2d.py
@@ -6,6 +6,8 @@ import torch
 import yaml
 from utils import *
 
+from PIL import ImageFont, ImageDraw, Image
+
 if __name__ == "__main__":
     with open("configs/default.yaml") as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
@@ -15,21 +17,29 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     mp_drawing = mp.solutions.drawing_utils
     mp_hands = mp.solutions.hands
-    cap = cv2.VideoCapture(4)
+    cap = cv2.VideoCapture(0)
+
+    w = 640
+    h = 480
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+
+    ui_img = make_ui_img(w)
 
     hands_keypoints = []
     translation_flag = False
     korean = [
-        "happiness",
-        "Hello",
-        "Sad",
-        "snow",
-        "you",
-        "eat",
-        "What's your name",
-        "Sign-Language",
-        "I Love You",
-        "Nice to meet you",
+        "행복",
+        "안녕",
+        "슬픔",
+        "눈",
+        "당신",
+        "식사하셨어요?",
+        "이름이 뭐 예요?"
+        #"수어",
+        #"사랑",
+        #"만나서 반가워요!",
     ]
     model = InferModel().load_from_checkpoint(checkpoint_path=ckpt_name)
     model.eval().to("cuda")
@@ -46,13 +56,6 @@ if __name__ == "__main__":
             results = hands.process(image)
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-            if display_flag:
-                display_label(labels, image)
-                label_play_time += 1
-            if label_play_time > 150:
-                label_play_time = 0
-                display_flag = False
 
             if results.multi_hand_landmarks:
                 if translation_flag and len(results.multi_handedness) == 2:
@@ -77,7 +80,18 @@ if __name__ == "__main__":
                 for num, hand in enumerate(results.multi_hand_landmarks):
                     mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
 
-            cv2.imshow("Hand Tracking", image)
+            image = drawUI(image, ui_img, np.zeros((100,w,3),np.uint8))
+
+            if display_flag:
+                image = display_label(labels, image, w, h)
+                label_play_time += 1
+
+            if label_play_time > 150:
+                label_play_time = 0
+                display_flag = False
+
+            cv2.imshow("Sign-Language Translator", image)
+
             key_queue = cv2.waitKey(1)
             if (key_queue & 0xFF == ord("s")) and translation_flag == False:
                 print("Start recording")
--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 import time
 import cv2
 from mediapipe.framework.formats import landmark_pb2
+from PIL import ImageFont, ImageDraw, Image
+import numpy as np
 
+
+fontpath = "/usr/share/fonts/truetype/nanum/NanumBarunpenB.ttf"
+font = ImageFont.truetype(fontpath, 50)
 
 def landmarkxy2list(
     landmarks_list: landmark_pb2.NormalizedLandmarkList,
@@ -23,8 +28,33 @@ def display_fps(pre_time, image):
     )
     return curr_time
 
+def make_ui_img(w):
+    ui_img = np.zeros((100,w,3),np.uint8)
+    
+    b,g,r,a = 255,255,255,0
 
-def display_label(label, image):
-    cv2.putText(
-        image, f"{label}", (200, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 0), 3
-    )
+    ui_img_pil = Image.fromarray(ui_img)
+    draw_top = ImageDraw.Draw(ui_img_pil)
+
+    guide_message = "카메라를 향해 수어를 해보세요!"
+
+    draw_top.text((w/2, 50), guide_message, anchor='mm', font=font, fill=(b,g,r,a))
+    ui_img = np.array(ui_img_pil)
+
+    return ui_img
+
+def drawUI(img, ui_img, subtitle_bg):
+    stacked_img = cv2.vconcat([ui_img, img])
+    stacked_img = cv2.vconcat([stacked_img, subtitle_bg])
+
+    return stacked_img
+
+def display_label(label, image, w, h):
+    b,g,r,a = 255,255,255,0
+
+    img_pil = Image.fromarray(image)
+    draw = ImageDraw.Draw(img_pil)
+    draw.text((w/2, h+100+50), label, anchor='mm', font=font, fill=(b,g,r,a))
+    img = np.array(img_pil)
+
+    return img
